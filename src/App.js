40db7dbad2f7cfb61648aa6fc9c7673d4a561535
vikas6577 <vs509411@gmail.com>
2023-03-04 02:42:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<h1> Product Listing Components</h1>}/>
+        <Route path="/add" element={<h1> Add Product Component</h1>}/>
+        <Route path="/update" element={<h1> Update Product</h1>}/>
+        <Route path="/logout" element={<h1> Logout Component</h1>}/>
+        <Route path="/profile" element={<h1> Profile Component</h1>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+    </div>
+  );
+}
+// 
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer=()=>{
+    return (
+        <div className="footer">
+            <h2>E-comm Dashboard</h2>
+        </div>
+    )
+}
+
+export default Footer; 
